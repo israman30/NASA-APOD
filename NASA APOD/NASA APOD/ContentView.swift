a7d7f310719d0cd20a51148306c1aadc588c793b
Israel Manzo <israelmanzo814@gmail.com>
2025-03-20 15:31:43 -0400

    adding network manager

--- NASA APOD/NASA APOD/ContentView.swift
+++ NASA APOD/NASA APOD/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// https://api.nasa.gov/planetary/apod?api_key=tXNvkBhAvgNMi8HR7pJX3PXbxUx3df95cMwiT2g0
+
 struct ContentView: View {
     var body: some View {
         NavigationView {
@@ -18,4 +20,91 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct Constant {
+    static var apiKey: String {
+        "tXNvkBhAvgNMi8HR7pJX3PXbxUx3df95cMwiT2g0"
+    }
+    
+    static var baseURL: String {
+        "https://api.nasa.gov/planetary/apod?"
+    }
+    
+    static var url: URL {
+        guard let url = URL(string: "\(baseURL)api_key=\(apiKey)") else {
+            fatalError("Invalid URL")
+        }
+        return url
+    }
+}
+
+struct APOD {
+    let date: String
+    let explanation: String
+    let hdurl: String
+    let media_type: String
+    let title: String
+    let url: String
+}
+
+enum NetworkError: Error {
+    case invalidateResponse
+    case decondingFailed
+    case clientError(statusCode: Int)
+    case serverError(statusCode: Int)
+    case unknownError(statusCode: Int)
+}
+
+extension NetworkError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidateResponse:
+            return "Invalid response"
+        case .decondingFailed:
+            return "Decoding failed"
+        case .clientError(statusCode: let code):
+            return "Client error: \(code)"
+        case .serverError(statusCode: let code):
+            return "Server error: \(code)"
+        case .unknownError:
+            return "Unknown error"
+        }
+    }
+}
+
+protocol NetworkManagerProtocol {
+    func fetch<T: Decodable>(from endpoint: URL) async throws -> T
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    func fetch<T: Decodable>(from endpoint: URL) async throws -> T {
+        let (data, response) = try await URLSession.shared.data(from: endpoint)
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw NetworkError.invalidateResponse
+        }
+        
+        try invalid(httpResponse)
+        
+        do {
+            let decode = JSONDecoder()
+            return try decode.decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decondingFailed
+        }
+    }
+    
+    func invalid(_ response: HTTPURLResponse) throws {
+        switch response.statusCode {
+        case 200...399:
+            return
+        case 400...599:
+            throw NetworkError.clientError(statusCode: response.statusCode)
+        case 600...799:
+            throw NetworkError.serverError(statusCode: response.statusCode)
+        default:
+            throw NetworkError.unknownError(statusCode: response.statusCode)
+        }
+    }
 }
