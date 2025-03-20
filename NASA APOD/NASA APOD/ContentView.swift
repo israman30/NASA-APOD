d04b0445706b8e48e773009e2d966353ca0c55ab
Israel Manzo <israelmanzo814@gmail.com>
2025-03-20 15:00:20 -0400

    added new project for NASA API

---
+++ NASA APOD/NASA APOD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NASA APOD
+//
+//  Created by Israel Manzo on 3/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello World!")
+                .navigationTitle("NASA APOD")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NASA APOD/NASA APOD/NASA_APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_APODApp.swift
+//  NASA APOD
+//
+//  Created by Israel Manzo on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct NASA_APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
