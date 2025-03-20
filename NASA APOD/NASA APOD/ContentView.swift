73d5f06732ecb3a36a74b400ebd5f6060f0289c6
Israel Manzo <israelmanzo814@gmail.com>
2025-03-20 19:44:56 -0400

    fetched api data

--- NASA APOD/NASA APOD/ContentView.swift
+++ NASA APOD/NASA APOD/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 // https://api.nasa.gov/planetary/apod?api_key=tXNvkBhAvgNMi8HR7pJX3PXbxUx3df95cMwiT2g0
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = APODViewModel(networkManger: NetworkManager())
+    
     var body: some View {
         NavigationView {
-            Text("Hello World!")
+            Text(viewModel.apod?.title ?? "nothing")
                 .navigationTitle("NASA APOD")
+                .task {
+                    await viewModel.fetchAPOD()
+                }
         }
     }
 }
@@ -24,4 +30,19 @@ struct ContentView: View {
 
 class APODViewModel: ObservableObject {
     @Published var apod: APOD?
+    @Published var error: NetworkError?
+    
+    private let networkManger: NetworkManager
+    
+    init(networkManger: NetworkManager) {
+        self.networkManger = networkManger
+    }
+    
+    func fetchAPOD() async {
+        do {
+            self.apod = try await networkManger.fetch(from: Constant.url)
+        } catch {
+            self.error = .unknownError(statusCode: 0)
+        }
+    }
 }
