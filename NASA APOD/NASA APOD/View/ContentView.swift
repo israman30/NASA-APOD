af1139acfccf6494dd51d974c9e14c09daf62b24
Israel Manzo <israelmanzo814@gmail.com>
2025-03-23 10:24:08 -0400

    update switching image and video

--- NASA APOD/NASA APOD/View/ContentView.swift
+++ NASA APOD/NASA APOD/View/ContentView.swift
@@ -26,51 +26,79 @@ struct ContentView: View {
                         await viewModel.fetchAPOD(with: currentDate.formatted(.iso8601.year().month().day()))
                     }
                 }
-                
-                VStack {
-                    if let urlString = viewModel.apod?.hdurl, let url = URL(string: urlString) {
-                        AsyncImage(url: url) { phase in
-                            switch phase {
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            case .empty:
-                                Image(systemName: "photo.artframe")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            case .failure(_):
-                                Image(systemName: "photo.artframe")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            @unknown default:
-                                Image(systemName: "photo.artframe")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            }
-                        }
-                    }
-                    
-                    Text(viewModel.apod?.title ?? "nothing")
-                        .font(.title2)
-                    
-                    Text(viewModel.apod?.explanation ?? "nothing")
-                        .font(.body)
-                    
-                    HStack {
-                        Spacer()
-                        Text(viewModel.apod?.date ?? "nothing")
-                            .font(.body)
-                    }
-                    Spacer()
-                }
-                .navigationTitle("NASA APOD")
-                .padding()
                 .task {
-                    await viewModel.fetchAPOD(with: viewModel.apod?.date ?? "")
+                    await viewModel.fetchAPOD(with: "2021-10-11")
+                }
+                
+                if viewModel.apod?.hdurl == nil && viewModel.apod?.media_type == "image" {
+                    imageType
+                } else {
+                    videotype
                 }
             }
         }
+    }
+    
+    private var imageType: some View {
+        VStack {
+            if let urlString = viewModel.apod?.hdurl, let url = URL(string: urlString) {
+                AsyncImage(url: url) { phase in
+                    switch phase {
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    case .empty:
+                        Image(systemName: "photo.artframe")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    case .failure(_):
+                        Image(systemName: "photo.artframe")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    @unknown default:
+                        Image(systemName: "photo.artframe")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                }
+            }
+            
+            Text(viewModel.apod?.title ?? "nothing")
+                .font(.title2)
+            
+            Text(viewModel.apod?.explanation ?? "nothing")
+                .font(.body)
+            
+            HStack {
+                Spacer()
+                Text(viewModel.apod?.date ?? "nothing")
+                    .font(.body)
+            }
+            Spacer()
+        }
+        .navigationTitle("NASA APOD")
+        .padding()
+    }
+    
+    private var videotype: some View {
+        VStack {
+            ViewPlayerView(videoURLString: viewModel.apod?.url)
+            
+            Text(viewModel.apod?.title ?? "nothing")
+                .font(.title2)
+            
+            Text(viewModel.apod?.explanation ?? "nothing")
+                .font(.body)
+            
+            HStack {
+                Spacer()
+                Text(viewModel.apod?.date ?? "nothing")
+                    .font(.body)
+            }
+            Spacer()
+        }
+        .padding()
     }
 }
 
--- NASA APOD/NASA APOD/View/ViewPlayerView.swift
+++ NASA APOD/NASA APOD/View/ViewPlayerView.swift
@@ -6,35 +6,19 @@
 //
 
 import SwiftUI
-import WebKit
+import AVKit
 
 struct ViewPlayerView: View {
     let videoURLString: String?
     
     var body: some View {
-        if let videoURLString = videoURLString {
-            ViewPlayerView(videoURLString: videoURLString)
+        if let videoString = videoURLString, let url = URL(string: videoString)  {
+            VideoPlayer(player: AVPlayer(url: url))
+                .scaledToFit()
         }
     }
 }
 
-struct VideoView: UIViewRepresentable {
-    
-    let videoURLString: String
-    
-    func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.configuration.allowsInlineMediaPlayback = true
-        webView.configuration.mediaTypesRequiringUserActionForPlayback = []
-        return webView
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        guard let url = URL(string: videoURLString) else { return }
-        uiView.scrollView.isScrollEnabled = false
-        uiView.load(URLRequest(url: url))
-    }
-}
 
 #Preview {
     ViewPlayerView(videoURLString: "https://www.youtube.com/embed/CC7OJ7gFLvE?rel=0")
